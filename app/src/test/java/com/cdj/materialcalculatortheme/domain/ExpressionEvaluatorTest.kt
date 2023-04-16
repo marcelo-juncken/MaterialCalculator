2f827f788b4d39e765e4e49822093b161950fbc6
Marcelo <rjmmarcelo@hotmail.com>
2023-04-16 16:19:19 -0300

    Added ExpressionEvaluator.kt

---
+++ app/src/test/java/com/cdj/materialcalculatortheme/domain/ExpressionEvaluatorTest.kt
@@ -0,0 +1,89 @@
+package com.cdj.materialcalculatortheme.domain
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class ExpressionEvaluatorTest {
+
+    private lateinit var evaluator: ExpressionEvaluator
+
+    @Test
+    fun `Simple expression properly evaluated`() {
+        evaluator = ExpressionEvaluator(
+            listOf(
+                ExpressionPart.Number(4.0),
+                ExpressionPart.Op(Operation.ADD),
+                ExpressionPart.Number(5.0),
+                ExpressionPart.Op(Operation.SUBTRACT),
+                ExpressionPart.Number(3.0),
+                ExpressionPart.Op(Operation.MULTIPLY),
+                ExpressionPart.Number(5.0),
+                ExpressionPart.Op(Operation.DIVIDE),
+                ExpressionPart.Number(3.0),
+
+                )
+        )
+
+        assertThat(evaluator.evaluate()).isEqualTo(4)
+    }
+
+    @Test
+    fun `Expression with decimals properly evaluated`() {
+        evaluator = ExpressionEvaluator(
+            listOf(
+                ExpressionPart.Number(4.5),
+                ExpressionPart.Op(Operation.ADD),
+                ExpressionPart.Number(5.5),
+                ExpressionPart.Op(Operation.SUBTRACT),
+                ExpressionPart.Number(3.5),
+                ExpressionPart.Op(Operation.MULTIPLY),
+                ExpressionPart.Number(5.5),
+                ExpressionPart.Op(Operation.DIVIDE),
+                ExpressionPart.Number(3.5),
+            )
+        )
+        assertThat(evaluator.evaluate()).isEqualTo(4.5)
+    }
+
+    @Test
+    fun `Expression with parentheses properly evaluated`() {
+        evaluator = ExpressionEvaluator(
+            listOf(
+                ExpressionPart.Number(4.5),
+                ExpressionPart.Op(Operation.ADD),
+                ExpressionPart.Parentheses(ParenthesesType.Opening),
+                ExpressionPart.Number(5.5),
+                ExpressionPart.Op(Operation.SUBTRACT),
+                ExpressionPart.Number(3.5),
+                ExpressionPart.Parentheses(ParenthesesType.Closing),
+                ExpressionPart.Op(Operation.MULTIPLY),
+                ExpressionPart.Number(5.5),
+                ExpressionPart.Op(Operation.DIVIDE),
+                ExpressionPart.Number(11.0),
+            )
+        )
+        assertThat(evaluator.evaluate()).isEqualTo(5.5)
+    }
+
+    @Test
+    fun `Expression with nested parentheses properly evaluated`() {
+        evaluator = ExpressionEvaluator(
+            listOf(
+                ExpressionPart.Number(4.5),
+                ExpressionPart.Op(Operation.ADD),
+                ExpressionPart.Parentheses(ParenthesesType.Opening),
+                ExpressionPart.Number(5.5),
+                ExpressionPart.Op(Operation.SUBTRACT),
+                ExpressionPart.Parentheses(ParenthesesType.Opening),
+                ExpressionPart.Number(3.5),
+                ExpressionPart.Op(Operation.MULTIPLY),
+                ExpressionPart.Number(5.5),
+                ExpressionPart.Parentheses(ParenthesesType.Closing),
+                ExpressionPart.Parentheses(ParenthesesType.Closing),
+                ExpressionPart.Op(Operation.DIVIDE),
+                ExpressionPart.Number(11.0),
+            )
+        )
+        assertThat(evaluator.evaluate()).isEqualTo(3.25)
+    }
+}
